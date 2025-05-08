ab6d1bbadc4e50ea5b5587d0314480386729a2ab
MichaelMalardDev <michaelmalard.dev@gmail.com>
2025-05-08 09:54:54 -0400

    doing-searchbarlogic

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,103 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import MovieCard from "@/components/MovieCard";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const [searchQuery, setSearchQuery] = useState("");
 
-export default Search
+  const {
+    data: movies,
+    loading: moviesLoading,
+    refetch: loadMovies,
+    error: moviesError,
+    reset,
+  } = useFetch(() => fetchMovies({ query: searchQuery }), false);
+
+  useEffect(() => {
+    const func = async () => {
+      if (searchQuery.trim()) {
+        await loadMovies();
+      } else {
+        reset();
+      }
+    };
+
+    func();
+  }, [searchQuery]);
+
+  return (
+    <View className='flex-1 bg-primary'>
+      <Image
+        source={images.bg}
+        className='flex-1 absolute w-full z-0'
+        resizeMode='cover'
+      />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        className='px-5'
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16,
+          marginVertical: 16,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        ListHeaderComponent={
+          <>
+            <View className='w-full flex-row justify-center mt-20 items-center'>
+              <Image
+                source={icons.logo}
+                className='w-12 h-10'
+              />
+            </View>
+
+            <View className='my-5'>
+              <SearchBar
+                placeholder='Search movies...'
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
+            </View>
+
+            {moviesLoading && (
+              <ActivityIndicator
+                size={"large"}
+                color={"#0000ff"}
+                className='my-3'
+              />
+            )}
+
+            {moviesError && (
+              <Text className='text-red-500 px-5 my-3'>
+                Error: {moviesError.message}
+              </Text>
+            )}
+
+            {!moviesLoading &&
+              !moviesError &&
+              searchQuery.trim() &&
+              movies?.length > 0 && (
+                <Text className='text-xl text-white font-bold'>
+                  Search Results for{" "}
+                  <Text className='text-accent'>{searchQuery}</Text>
+                </Text>
+              )}
+          </>
+        }
+      />
+    </View>
+  );
+};
+
+export default Search;
